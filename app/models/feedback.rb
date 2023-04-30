876ce2168d5bc2673cef08f3d41e5259a116a956
ajrod-dev <alexander.joshua.rodriguez@gmail.com>
2023-04-30 12:01:35 -0600

    added validations for recipient_handle and text

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,13 @@
 class Feedback < ApplicationRecord
-
+  before_create :sanitize
   after_create :send_tweet
+
+  validates_presence_of :recipient_handle, message: "Cannot be blank. Need a recipient to send it to."
+  validates_length_of :text, minimum: 1, maximum: 250
+
+  def sanitize
+    self.recipient_handle = self.recipient_handle.gsub('@', '')
+  end
 
   def send_tweet
     TwitterService.tweet!(self)
